6099e4177c6ab26f60c274dcc401e21c41f121f0
WilliamRausch <nolanrausch@Gmail.com>
2018-08-25 16:21:30 -0400

    first version finished

--- src/App.js
+++ src/App.js
@@ -2,17 +2,117 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      Genre: ""
+    }
+    this.handleSearch = this.handleSearch.bind(this);
+    this.playMusic =  this.playMusic.bind(this);
+    
+  }
+  componentDidMount(){
+   
+   
+  }
+   handleSearch(genre) {
+
+    this.setState({
+      Genre: genre
+    })
+    console.log(genre);
+
+    // let search = this.state.Genre;
+
+    //  let url = ("https://itunes.apple.com/search?term=" + search)
+
+    // fetch(url)
+    //     .then(function(response){
+
+    //       response.json().then(function(data){
+    //       console.log(data.resultCount);
+    //       console.log(data);
+    //       let randomSelector = Math.floor(Math.random() * Math.floor(50));
+          
+    //       let player = document.getElementById('music-player');
+    //       let musicPreview = data.results[randomSelector].previewUrl;
+    //       console.log(musicPreview);
+    //              player.setAttribute('src', musicPreview);
+    //              player.play();
+    //     })
+    //     })
+    if(genre){
+    this.playMusic(genre);
+  }
+
+
+
+
+  }
+  playMusic(genre){
+    console.log(this.state.Genre + "2");
+     let search = genre;
+
+     if(search){
+
+     let url = ("https://itunes.apple.com/search?term=" + search)
+
+    fetch(url)
+        .then(function(response){
+
+          response.json().then(function(data){
+          console.log(data.resultCount);
+          console.log(data);
+          let randomSelector = Math.floor(Math.random() * Math.floor(50));
+          
+          let player = document.getElementById('music-player');
+          let musicPreview = data.results[randomSelector].previewUrl;
+          console.log(musicPreview);
+                 player.setAttribute('src', musicPreview);
+                 player.play();
+        })
+        })
+      }
+  }
+
+                
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="container">
+      <div id="Header">
+      <h1 id = "Title">Music App!</h1>
+       <section class="player">
+      <audio id="music-player" controls="controls" src=""></audio>
+    </section>
+      </div>
+      <div id="Genres">
+      <div id="country"   onClick={() => { this.handleSearch("country") }} >
+      <img src='https://static.thenounproject.com/png/96504-200.png'/>
+      <h1>Country </h1>  
+      
+      </div>
+      <div id="rock"   onClick={() => { this.handleSearch("rock") }}>
+      <img src='https://png.icons8.com/metro/1600/guitar.png'/>
+      <h1>Rock</h1>   
+      
+      </div>
+      <div id="jazz"   onClick={() => { this.handleSearch("jazz") }}>
+      <img src="http://icons.iconarchive.com/icons/icons8/ios7/256/Music-Saxophone-icon.png"/>
+      <h1>Jazz</h1> 
+      
+      </div>
+      <div id="rap"   onClick={() => { this.handleSearch("rap") }}>
+      <img src="https://cdn2.iconfinder.com/data/icons/people-3-2/128/Rapper-Rap-Musician-Hip-Hop-Urban-Man-512.png"/>
+      <h1>Rap</h1> 
+      
+      </div>
+
+      </div>
       </div>
     );
   }
